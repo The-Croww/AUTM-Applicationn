d03b3443091e57fdb7908cd1e24cdd2066ba4262
joshuamarcaida <zymonjoshuamarcaida@gmail.com>
2026-06-12 10:59:16 +0800

    my changes

---
+++ android/automato_esp32/secrets.h
@@ -0,0 +1,5 @@
+#pragma once
+#define WIFI_SSID       "ZTE2.4"
+#define WIFI_PASSWORD   "your-wifi-password"
+#define DATABASE_URL    "https://automato-7ed8e-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define DATABASE_SECRET "your-NEW-regenerated-secret"
