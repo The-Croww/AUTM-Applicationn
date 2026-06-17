21c116868412253efc0ab274db71df654561cbd3
Louis Manuel R. Cacho <The-Croww>
2026-06-18 03:27:06 +0800

    auth adjusted

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.automato.greenhouse"
+    namespace = "com.example.autm_application02"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.automato.greenhouse"
+        applicationId = "com.example.autm_application02"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
